b964642b4665b48fce86c444ce72a86b2f932e07
Rishu-kr <76037740+Rishu-kr@users.noreply.github.com>
2023-08-21 17:23:17 +0530

    Update Task3_Simple_Calculator (2).cpp

--- Task3_Simple_Calculator (2).cpp
+++ Task3_Simple_Calculator (2).cpp
@@ -1,41 +1,47 @@
 //Intern Name -Rishabh Kumar 
 #include<iostream>
-
 using namespace std;
-
+//main function
 int main() {
     double num1, num2;
     char operation;
-
+//enter first name
     cout << "Simple Calculator!" << endl;
     cout << "Enter the First Number: ";
     cin >> num1;
+    // enter second name
     cout << "Enter the Second Number: ";
     cin >> num2;
-
-    cout << "Choose an operation (+, -, *, /): ";
+//pick any operation
+    cout << "pick operation (+, -, *, /): ";
     cin >> operation;
-
+//perform the task 
     switch (operation) {
+        //case 1
         case '+':
             cout << "Result: " << num1 + num2 << endl;
             break;
+        //case 2
         case '-':
             cout << "Result: " << num1 - num2 << endl;
             break;
+        //case 3
         case '*':
             cout << "Result: " << num1 * num2 << endl;
             break;
+        //case 4
         case '/':
             if (num2 == 0) {
-                cout << "Error: Division by zero is not allowed." << endl;
+                //zero cant be used as a divisor
+                cout << "Error: Division by zero is invalid." << endl;
             } else {
                 cout << "Result: " << num1 / num2 << endl;
             }
             break;
         default:
-            cout << "Operation does not exist.Please choose a valid operation." << endl;
+            //invalid operation then choose valid one
+            cout << "Operation invalid.Please choose a valid operation." << endl;
     }
-
+//that is the code 
     return 0;
 }
