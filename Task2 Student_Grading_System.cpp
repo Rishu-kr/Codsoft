b73cb0cb6bec53811962909e6fec6a28275cb649
Rishu-kr <76037740+Rishu-kr@users.noreply.github.com>
2023-08-21 17:20:28 +0530

    Update Task2 Student_Grading_System.cpp

--- Task2 Student_Grading_System.cpp
+++ Task2 Student_Grading_System.cpp
@@ -1,8 +1,9 @@
-
+//intern Name -Rishabh Kumar 
+//Codsoft 
 #include <iostream>
 #include <string>
 #include <vector>
-
+//import all the lib
 using namespace std;
 
 struct Student {
@@ -12,45 +13,43 @@ struct Student {
 
 int main() {
     vector<Student> students;
-    int numStudents;
-
-    cout << "Enter the total number of students: ";
-    cin >> numStudents;
-
-    // Input the student names and their corresponding grades.
-    for (int i = 0; i < numStudents; ++i) {
+    int numStu;
+//enter the total no of students 
+    cout << "Enter total number of students: ";
+    cin >> numStu;
+    //using a for loop
+    for (int i = 0; i < numStu; ++i) {
         Student student;
-        cout << "Enter the name of student " << i + 1 << ": "<<endl;
+        //enter the name 
+        cout << "Enter name of student " << i + 1 << ": "<<endl;
         cin >> student.name;
-        cout << "Enter the grade of student " << i + 1 << ": "<<endl;
+        //enter the grade 
+        cout << "Enter grade of student " << i + 1 << ": "<<endl;
         cin >> student.grade;
         students.push_back(student);
     }
-
-    // Calculate the average grade
-    double sumGrades = 0.0;
+    double sumOfGrad = 0.0;
     for (const auto& student : students) {
-        sumGrades += student.grade;
+        sumOfGrad += student.grade;
     }
-    double averageGrade = sumGrades / numStudents;
-
-    // Find highest and lowest grades
-    double highestGrade = students[0].grade;
-    double lowestGrade = students[0].grade;
+    double averageGrade = sumOfGrad / numStu;
+     // fiding lowest grade 
+     double lG = students[0].grade; //lG= Lowest Grade
+    //finding highest grade
+    double hG = students[0].grade; //hG=highest grade
     for (const auto& student : students) {
-        highestGrade = max(highestGrade, student.grade);
-        lowestGrade = min(lowestGrade, student.grade);
+        hG = max(hG, student.grade);
+        lG = min(lG, student.grade);
     }
-
-    // Display results
     cout << "\nStudent Grades Summary:\n";
     for (const auto& student : students) {
         cout << student.name << ": " << student.grade << endl;
     }
-
+//print the grades average ,highest and lowest
     cout << "\nAverage Grade: " << averageGrade << endl;
-    cout << "Highest Grade: " << highestGrade << endl;
-    cout << "Lowest Grade: " << lowestGrade << endl;
-
+    cout << "hG: " << highestGrade << endl;
+    cout << "lG: " << lowestGrade << endl;
+//print the grades 
+    
     return 0;
 }
