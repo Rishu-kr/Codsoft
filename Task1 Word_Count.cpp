75fb94804bb9202abb936844be99ec73459bcd3a
Rishu-kr <76037740+Rishu-kr@users.noreply.github.com>
2023-08-21 17:13:03 +0530

    Update Task1 Word_Count.cpp

--- Task1 Word_Count.cpp
+++ Task1 Word_Count.cpp
@@ -1,39 +1,43 @@
-
+//Intern Name-Rishabh Kumar # Codsoft #word counter 
+//import all the libraries 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <sstream>
-
+//This is the code for the the task 1 word count in C++ of Codsoft Internship.
 using namespace std;
-
+//to count words
 int countWords(const string& filename) {
     ifstream file(filename);
     if (!file) {
-        cout << "Error during opening file: " << filename << endl;
+        //if error during file opening.
+        cout << "Error in opening file: " << filename << endl;
         return -1;
     }
-    string accesstext;
-    int wordCount = 0;
-    while(getline(file, accesstext)) {
-        istringstream inputword(accesstext);
+    string acctext;
+    int wordCount = 0; //initiate a counter to count the word .
+    while(getline(file, acctext)) {
         string word;
+        istringstream inputword(acctext);
         while (inputword >> word) {
-            wordCount++;
+            wordCount++; // increment the counter
         }
     }
 
-    return wordCount;
+    return wordCount; 
+    //finally return the value of word count
 }
 
 int main() {
     string filename;
     cout << "Enter Name of the File: ";
-    getline(cin, filename);
+    getline(cin, filename); //fetch the file
 
     cout<<endl;
-    int totalWords = countWords(filename);
-    if (totalWords != -1) {
-        cout << "Total number of words in the file: " << totalWords << endl;
+    int allWords = countWords(filename); //call the function word count
+    //to count all words in file
+    if (allWords != -1) {
+        cout << "Total number of words in the file: " << allWords << endl;
         cout<<endl;
     }
 
